2a53c1ac8fad12d0441ed753174e2b05dd38de1d
MichaelWiss <michael.wiss@gmail.com>
2017-01-30 13:47:15 -0600

    express testing

--- server/server.js
+++ server/server.js
@@ -3,8 +3,21 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res)=> {
-	res.send('Hello world!');
+	res.status(404).send({
+		error: 'Page not found.'
+	});
 });
+
+app.get('/users', (req, res)=> {
+	res.status(404).send([{
+		name: 'Mike',
+		age: 45,
+	}, {
+		name: 'Andrew',
+		age: 25
+	}])	
+});
+
 
 app.listen(3000);
 module.exports.app = app;
