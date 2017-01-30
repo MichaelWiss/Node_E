64d035859652b726589946f2c76c5c07bca674f4
MichaelWiss <michael.wiss@gmail.com>
2017-01-30 11:42:55 -0600

    writing async tests

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -12,11 +12,25 @@ expect(res).toBe(44).toBeA('number');
 // }
 });
 
+it('should async add two numbers', (done) => {
+   utils.asyncAdd(4, 3, (sum) => {
+     expect(sum).toBe(7).toBeA('number');
+     done();
+   });
+});
+
 
 
 it('should produce the square of the number', ()=> {
 	var res = utils.square(4);
     expect(res).toBe(16).toBeA('number');
+});
+
+it('should async square a number', (done)=> {
+	utils.asyncSquare(5, (product) => {
+		expect(product).toBe(25).toBeA('number');
+		done();
+	});
 });
 
 it('should have two strings', ()=> {
