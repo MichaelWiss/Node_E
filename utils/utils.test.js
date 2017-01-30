89d7a75ec143b8ee8b3bbd9827aa430d90fd741b
MichaelWiss <michael.wiss@gmail.com>
2017-01-29 18:37:53 -0600

    expect

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,19 +1,22 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', ()=> {
   var res = utils.add(33,11);
 
-  if (res !== 44) {
-  throw new Error(`Expected 44, but got ${res}.`)
-}
+expect(res).toBe(44).toBeA('number');
+
+//   if (res !== 44) {
+//   throw new Error(`Expected 44, but got ${res}.`)
+// }
 });
+
+
 
 it('should produce the square of the number', ()=> {
 	var res = utils.square(4);
-
-	if (res != 16) {
-	throw new Error(`Expected 16, but got ${res}.`)
-}
+    expect(res).toBe(16).toBeA('number');
 });
 
 
